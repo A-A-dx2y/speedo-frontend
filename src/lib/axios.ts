45c40f3b3dfab42fa2c21502ace01a478a233c1b
achuasokan <achuasokan555@gmail.com>
2026-05-19 17:09:53 +0530

    resolved te issue in the refresh token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { AUTH_ENDPOINTS } from '../constants/api/auth';
 import { logout } from '../store/slices/authSlice.js';
 import { store } from '../store';
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
   withCredentials: true,
@@ -10,22 +11,34 @@ const api = axios.create({
   },
 });
 
-// Interceptor to handle expired tokens
 api.interceptors.response.use(
   (response) => response,
+
   async (error) => {
+
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    // STOP infinite loop
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      originalRequest.url !== AUTH_ENDPOINTS.REFRESH
+    ) {
+
       originalRequest._retry = true;
 
       try {
+
         await api.post(AUTH_ENDPOINTS.REFRESH);
 
         return api(originalRequest);
+
       } catch (refreshError) {
+
         store.dispatch(logout());
+
         window.location.href = '/login';
+
         return Promise.reject(refreshError);
       }
     }
